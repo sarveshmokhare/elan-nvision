bdcd94f2bc84baf399f86f9a78bef4ff49bc6b32
Himanshu Kumar Gupta <ai21btech11012@gmail.com>
2022-10-04 09:01:03 +0530

    changing footer and compe page

--- js/event.js
+++ js/event.js
@@ -15,24 +15,50 @@ function slide1() {
     techy = document.getElementById("techy")
     imageCarousel = document.getElementById("image_carousel");
 
-    if (!culti.classList.contains("active")) {
+    if(!culti.classList.contains("active") && techy.classList.contains("active")){
+        techy.classList.remove("active")
+        techy.classList.add("inactive")
+        culti.classList.remove("inactive")
+        culti.classList.add("active")
+        culti.style.zIndex=1
+        setTimeout(
+            function remove(){
+                techy.classList.remove("inactive")
+            },2000
+        )
+        
+    }
+
+    else if (!culti.classList.contains("active")) {
+        culti.classList.remove("inactive");
         culti.classList.add("active");
         
         culti.style.zIndex = 1;
-        imageCarousel.style.zIndex = -1;
+        imageCarousel.style.zIndex = 0;
         techy.style.zIndex = -1;
         techy.classList.remove("active");
     }
-    else if (culti.classList.contains("active") && techy.classList.contains("active")) {
-        culti.classList.remove("active");
-        culti.style.zIndex = -1;
+    // else if (culti.classList.contains("active") && techy.classList.contains("active")) {
+    //     culti.classList.remove("active");
+    //     culti.classList.add("inactive")
+    //     setTimeout(
+    //         function remove(){
+    //             culti.classList.remove("inactive")
+    //         },2000
+    //     )
+    //     culti.style.zIndex = -1;
         
-        imageCarousel.style.zIndex = 1;
-    }
+    //     imageCarousel.style.zIndex = 1;
+    // }
     else if (culti.classList.contains("active")) {
+        culti.classList.add("inactive");
         culti.classList.remove("active");
-        
-        imageCarousel.style.zIndex = 1;
+        setTimeout(
+            function remove(){
+                culti.classList.remove("inactive")
+            },2000
+        )
+        imageCarousel.style.zIndex = 0;
         
     }
 }
@@ -42,34 +68,77 @@ function slide2() {
     techy = document.getElementById("techy");
     culti = document.getElementById("culti");
     imageCarousel = document.getElementById("image_carousel");
-
-    if (!techy.classList.contains("active")) {
+    if(!techy.classList.contains("active") && culti.classList.contains("active")){
+        culti.classList.remove("active")
+        culti.classList.add("inactive")
+        techy.classList.remove("inactive")
+        techy.classList.add("active")
+        techy.style.zIndex=1
+        setTimeout(
+            function remove(){
+                culti.classList.remove("inactive")
+            },2000
+        )
+        
+    }
+    else if (!techy.classList.contains("active")) {
+        techy.classList.remove("inactive")
         techy.classList.add("active");
 
         techy.style.zIndex = 1;
-        imageCarousel.style.zIndex = -1;
+        imageCarousel.style.zIndex = 0;
         culti.style.zIndex = -1;
         culti.classList.remove("active");
     }
-    else if (techy.classList.contains("active") && culti.classList.contains("active")) {
-        techy.classList.remove("active");
-        techy.style.zIndex = -1;
+    // else if (techy.classList.contains("active") && culti.classList.contains("active")) {
+    //     techy.classList.remove("active");
+    //     techy.classList.add("inactive")
+    //     setTimeout(
+    //         function remove(){
+    //             techy.classList.remove("inactive")
+    //         },2000
+    //     )
+    //     techy.style.zIndex = -1;
     
-        imageCarousel.style.zIndex = 1;
-    }
+    //     imageCarousel.style.zIndex = 1;
+    // }
     else if (techy.classList.contains("active")) {
         techy.classList.remove("active");
+        techy.classList.add("inactive")
+        setTimeout(
+            function remove(){
+                techy.classList.remove("inactive")
+            },2000
+        )
         
-        imageCarousel.style.zIndex = 1;
+        imageCarousel.style.zIndex = 0;
         
     }
 }
 // document.getElementById("vert-navbar").style.width=document.body.clientWidth
 
 function home() {
+    // slide1()
+    // slide2()
     techy = document.getElementById("techy")
     culti = document.getElementById("culti")
-    if (techy.classList.contains("active")) techy.classList.remove("active")
-    if (culti.classList.contains("active")) culti.classList.remove("active")
+    if (techy.classList.contains("active")) {
+        techy.classList.remove("active")
+        techy.classList.add("inactive")
+        setTimeout(
+            function remove(){
+                techy.classList.remove("inactive")
+            },2000
+        )
+    }
+    if (culti.classList.contains("active")){ 
+        culti.classList.remove("active")
+        culti.classList.add("inactive")
+        setTimeout(
+            function remove(){
+                culti.classList.remove("inactive")
+            },2000
+        )
+    }
     document.getElementById("image_carousel").style.zIndex = 0;
 }
